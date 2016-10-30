a627d26ae6b218ef9de41bd3542d0c5be5f93c2e
07150709chenyumin <1039163268@qq.com>
2016-10-30 19:57:56 +0800

    Initial commit

---
+++ app/src/main/java/a07150709/gdmec/intent/intentDemo1.java
@@ -0,0 +1,24 @@
+package a07150709.gdmec.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class intentDemo1 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intentdemo1);
+    }
+    public void upper(View v){
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        final String value =bundle.getString("value").toUpperCase();
+        Intent intentresult = new Intent();
+        intentresult.putExtra("result",value);
+        setResult(RESULT_OK,intentresult);
+        finish();
+    }
+}
